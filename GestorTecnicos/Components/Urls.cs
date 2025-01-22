264eb8f74cbac532154b243366fa20a9e41a940c
Esthiber <esthiber1@gmail.com>
2025-01-22 08:59:16 -0400

    Se definio que hace la clase URL closes #5

--- GestorTecnicos/Components/Urls.cs
+++ GestorTecnicos/Components/Urls.cs
@@ -1,9 +1,9 @@
 ﻿namespace GestorTecnicos.Components
 {
     public static class Urls
-    {
+    { // Referencias URL de las páginas de la aplicación
         public static string Home => "/";
-        public static string TecnicosIndex => "/tecnicos";
+        public static string TecnicosIndex => "/tecnicos"; // URL de la página de inicial de la lista de técnicos
         public static string TecnicosCrear => "/tecnicos/create";
         public static string TecnicosEditar => "/tecnicos/edit";
     }
