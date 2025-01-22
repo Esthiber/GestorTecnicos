606b9d608274410ec4b84222b8a236f150dc32a0
Esthiber <esthiber1@gmail.com>
2025-01-22 08:47:46 -0400

    Correccion de RegularExpression para permitir Acentuacion y ñ closes #4

--- GestorTecnicos/Models/Tecnicos.cs
+++ GestorTecnicos/Models/Tecnicos.cs
@@ -7,7 +7,7 @@ namespace GestorTecnicos.Models
         [Key]
         public int TecnicoId { get; set; }
         [Required(ErrorMessage = "El Nombre es requerido")]
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
         public string? Nombres { get; set; } = null;
         [Required(ErrorMessage = "El Sueldo es requerido")]
         [Range(minimum: 0.1, maximum: 5000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a 5000 ")]
