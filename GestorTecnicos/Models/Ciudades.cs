a2116b0c22042533f68a589dc20bdadb1a617c1f
Esthiber <esthiber1@gmail.com>
2025-01-27 18:27:06 -0400

    Creacion del Modelo Ciudades

---
+++ GestorTecnicos/Models/Ciudades.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorTecnicos.Models
+{
+    public class Ciudades
+    {
+        [Key]
+        private int CiudadId { get; set; }
+        [Required(ErrorMessage="Debe ingresar el nombre de ciudad.")]
+        [RegularExpression(@"^(?!\s*$).{2,}$", ErrorMessage = "El nombre debe tener al menos 2 caracteres y no puede contener solo espacios en blanco.")]
+        private string? Nombre { get; set; }
+    }
+}
