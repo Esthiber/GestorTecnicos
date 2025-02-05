803ea6914a4b66b6550d47a384e804a2cbe6e5fa
Esthiber <esthiber1@gmail.com>
2025-02-05 08:18:38 -0400

    Injeccion del `SistemasService` en `Program`

--- GestorTecnicos/Program.cs
+++ GestorTecnicos/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddScoped<TecnicosService>();
 builder.Services.AddScoped<ClientesService>();
 builder.Services.AddScoped<CiudadesService>();
 builder.Services.AddScoped<TicketsService>();
+builder.Services.AddScoped<SistemasService>();
 
 var app = builder.Build();
 
